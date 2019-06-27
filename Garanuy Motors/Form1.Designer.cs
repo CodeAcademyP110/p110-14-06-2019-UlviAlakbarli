4dc49757d201f98308912f1511f4ce7482fe1ffb
UlviAlakbarli <ulvialakbarli@gmail.com>
2019-06-27 12:19:25 +0400

    Add

---
+++ Garanuy Motors/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Garanuy_Motors
+{
+    partial class GaranuyMotors
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbBrands = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbModels = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "GARANUY MOTORS";
+            // 
+            // cmbBrands
+            // 
+            this.cmbBrands.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbBrands.FormattingEnabled = true;
+            this.cmbBrands.Location = new System.Drawing.Point(29, 121);
+            this.cmbBrands.Name = "cmbBrands";
+            this.cmbBrands.Size = new System.Drawing.Size(121, 21);
+            this.cmbBrands.TabIndex = 1;
+            this.cmbBrands.SelectedIndexChanged += new System.EventHandler(this.CmbBrands_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Brands";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(182, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Models";
+            // 
+            // cmbModels
+            // 
+            this.cmbModels.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbModels.FormattingEnabled = true;
+            this.cmbModels.Location = new System.Drawing.Point(185, 121);
+            this.cmbModels.Name = "cmbModels";
+            this.cmbModels.Size = new System.Drawing.Size(121, 21);
+            this.cmbModels.TabIndex = 4;
+            // 
+            // GaranuyMotors
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbModels);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbBrands);
+            this.Controls.Add(this.label1);
+            this.Name = "GaranuyMotors";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.GaranuyMotors_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbBrands;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbModels;
+    }
+}
+
---
+++ Garanuy Motors/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Garanuy_Motors
+{
+    public partial class GaranuyMotors : Form
+    {
+        public GaranuyMotors()
+        {
+            InitializeComponent();
+        }
+
+        private void GaranuyMotors_Load(object sender, EventArgs e)
+        {
+            string constrng = ConfigurationManager.ConnectionStrings["default"].ConnectionString;
+            using (SqlConnection Conn = new SqlConnection(constrng))
+            {
+               
+                string coman = "Select Name from Brand";
+                using(SqlCommand comando=new SqlCommand(coman, Conn))
+                {
+                    Conn.Open();
+                    SqlDataReader read = comando.ExecuteReader();
+                    if (read.HasRows)
+                    {
+                        while (read.Read())
+                        {
+                            cmbBrands.Items.Add(read[0].ToString());
+                        }
+                        cmbBrands.SelectedIndex = 0;
+                    }
+                }
+            }
+        }
+
+        private void CmbBrands_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string cost = ConfigurationManager.ConnectionStrings["default"].ConnectionString;
+            using(SqlConnection con=new SqlConnection(cost))
+            {
+                string comst="select Model.Name from Model "+
+                               "join Brand on Brand.Id = Model.BrandId "+
+                              $"where Brand.Name = '{cmbBrands.SelectedItem.ToString()}'";
+                using(SqlCommand combo=new SqlCommand(comst, con))
+                {
+                    con.Open();
+                    SqlDataReader redr = combo.ExecuteReader();
+                    if (redr.HasRows)
+                    {
+                        cmbModels.Items.Clear();
+                        while (redr.Read())
+                        {
+                            cmbModels.Items.Add(redr[0].ToString());
+                        }
+                        cmbModels.SelectedItem = 0;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Data is not found");
+                    }
+                }
+            }
+            cmbModels.SelectedIndex = 0;
+        }
+    }
+}
